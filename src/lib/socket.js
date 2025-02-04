9045e5fb7eda956da1157c159bf0dd9b325c079b
ashiq tom <ashiqtom6@gmail.com>
2025-02-04 17:57:54 +0530

    s

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,37 +1,13 @@
-import { Server } from "socket.io";
-import http from "http";
-import express from "express";
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
+const port = 3000;
 
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_URL,
-  },
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-export function getReceiverSocketId(userId) {
-  return userSocketMap[userId];
-}
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
-io.on("connection", (socket) => {
-  // console.log("A user connected", socket.id);
-
-  const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
-
-  // io.emit() is used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-  socket.on("disconnect", () => {
-    // console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
-  });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
-export { io, app, server };
