449f5d7d07704f144beb3d4d159c5910278e8cf1
ashiq tom <ashiqtom6@gmail.com>
2025-02-04 17:56:00 +0530

    rg

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
-import { connectDB } from "./lib/db.js";
-
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5001;
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: true, 
-    credentials: true, 
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-  })
-);
-
+const PORT = 5001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
-  connectDB();
 });
